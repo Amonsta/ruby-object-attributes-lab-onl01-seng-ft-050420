a7e1ea314bc99f660a67e2f8dd553afd855c4741
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-15 00:54:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,21 @@
 
 
 class Person 
-end
+  
+  def name=(persons_name)
+    @name = persons_name
+  end 
+  
+  def name 
+    @name
+  end 
+  
+   def job=(persons_job)
+    @job = persons_job
+  end 
+  
+  def job 
+    @job
+  end 
+
+end 
